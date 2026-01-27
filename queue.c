572e35bd030f9ed4ae7de46353176e601ad66b25
eeeXun <sdes96303@gmail.com>
2026-01-27 14:51:41 +0000

    Implement q_remove_head and q_remove_tail
    Use list_first_entry and list_last_entry to get the element. Then delete
    the corresponding list_head
    
    Change-Id: I203c90b18938d1cd15c0cd19c5e861fb0a012953

--- queue.c
+++ queue.c
@@ -47,12 +47,22 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+    element_t *node = list_first_entry(head, element_t, list);
+    strncpy(sp, node->value, bufsize - 1);
+    list_del(&node->list);
+    return node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
+    if (!head)
+        return NULL;
+    element_t *node = list_last_entry(head, element_t, list);
+    strncpy(sp, node->value, bufsize - 1);
+    list_del(&node->list);
     return NULL;
 }
 
