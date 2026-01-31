622f34346e14a2666de0235ec6bae457c6c89f27
eeeXun <sdes96303@gmail.com>
2026-01-31 09:33:34 +0000

    Fix q_size implementation
    Count the size with iterating each entry instead of iterating each list
    head
    
    Change-Id: Icbdf8050fdc509d410d63529091d16d614b754ba

--- queue.c
+++ queue.c
@@ -78,8 +78,8 @@ int q_size(struct list_head *head)
     if (!head)
         return 0;
     int len = 0;
-    struct list_head *node;
-    list_for_each(node, head)
+    element_t *entry;
+    list_for_each_entry(entry, head, list)
         len++;
     return len;
 }
