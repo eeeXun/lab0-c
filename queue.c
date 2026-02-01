814c82f926faaccba221e45f40f9471720b01198
eeeXun <sdes96303@gmail.com>
2026-02-01 06:48:35 +0000

    Fix remove element
    We should test if list is empty before removing element
    
    Change-Id: I650c99e0c9e91ebd62dba12bd78ce6e507fc562e

--- queue.c
+++ queue.c
@@ -61,7 +61,7 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head)
+    if (!head || list_empty(head))
         return NULL;
     element_t *node = list_first_entry(head, element_t, list);
     if (sp) {
@@ -75,7 +75,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    if (!head)
+    if (!head || list_empty(head))
         return NULL;
     element_t *node = list_last_entry(head, element_t, list);
     if (sp) {
