ac8fa999b88bafdd40a720ff74aea2dde44d874b
eeeXun <sdes96303@gmail.com>
2026-02-08 01:56:53 +0000

    Implement q_delete_dup
    Iterative to delete duplicated node
    
    Change-Id: I695acdc9da16f66680784e8ac52673a55b704bc6

--- queue.c
+++ queue.c
@@ -124,7 +124,21 @@ bool q_delete_mid(struct list_head *head)
 /* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head || list_empty(head))
+        return false;
+    bool duplicate = false;
+    element_t *entry, *safe;
+    list_for_each_entry_safe(entry, safe, head, list) {
+        if (&safe->list != head && strcmp(entry->value, safe->value) == 0) {
+            duplicate = true;
+            list_del(&entry->list);
+            q_release_element(entry);
+        } else if (duplicate) {
+            duplicate = false;
+            list_del(&entry->list);
+            q_release_element(entry);
+        }
+    }
     return true;
 }
 
